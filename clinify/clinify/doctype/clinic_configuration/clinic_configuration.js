934eb8bb67fc4294fdb115b1b60ab2a4312a1f61
Clinify <asifnb7@gmail.com>
2026-08-21 23:17:13 +0530

    feat: add platform admin clinic configuration

--- clinify/clinify/doctype/clinic_configuration/clinic_configuration.js
+++ clinify/clinify/doctype/clinic_configuration/clinic_configuration.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2026, Salniz Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Clinic Configuration", {
-// 	refresh(frm) {
+frappe.ui.form.on("Clinic Configuration", {
+	validate(frm) {
+		const clinic_statuses = ["Active", "Inactive", "Suspended"];
+		const subscription_statuses = ["Trial", "Active", "Expired", "Suspended"];
 
-// 	},
-// });
+		if (!clinic_statuses.includes(frm.doc.clinic_status)) {
+			frappe.throw(__("Please select a valid Clinic Status."));
+		}
+
+		if (!subscription_statuses.includes(frm.doc.subscription_status)) {
+			frappe.throw(__("Please select a valid Subscription Status."));
+		}
+
+		if (frm.doc.clinic_status === "Active" && !frm.doc.activation_date) {
+			frappe.throw(__("Activation Date is required for an active clinic."));
+		}
+	},
+});
--- clinify/clinify/doctype/clinic_configuration/test_clinic_configuration.py
+++ clinify/clinify/doctype/clinic_configuration/test_clinic_configuration.py
@@ -1,9 +1,39 @@
 # Copyright (c) 2026, Salniz Technologies and Contributors
 # See license.txt
 
-# import frappe
+import json
+from pathlib import Path
+
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestClinicConfiguration(FrappeTestCase):
-	pass
+	@classmethod
+	def setUpClass(cls):
+		super().setUpClass()
+		cls.meta = frappe.get_meta("Clinic Configuration")
+		cls.form_script = Path(__file__).with_name("clinic_configuration.js").read_text()
+
+	def test_required_identity_field(self):
+		self.assertTrue(self.meta.get_field("clinic_name").reqd)
+
+	def test_platform_status_options(self):
+		self.assertEqual(
+			self.meta.get_field("clinic_status").options.splitlines()[1:],
+			["Active", "Inactive", "Suspended"],
+		)
+		self.assertEqual(
+			self.meta.get_field("subscription_status").options.splitlines()[1:],
+			["Trial", "Active", "Expired", "Suspended"],
+		)
+
+	def test_activation_date_field_and_active_rule(self):
+		activation_date = self.meta.get_field("activation_date")
+
+		self.assertEqual(activation_date.fieldtype, "Date")
+		self.assertIn('frm.doc.clinic_status === "Active"', self.form_script)
+		self.assertIn("!frm.doc.activation_date", self.form_script)
+
+	def test_form_script_is_valid_json_free_javascript(self):
+		self.assertNotIn("clinic_code", self.form_script.split("validate", 1)[1])
